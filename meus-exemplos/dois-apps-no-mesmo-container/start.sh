c2475d7bf374b153a56d64868c5e39862489d050
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2021-10-27 14:53:04 -0300

    Dockerfile que sobe 1 app SpringBoot(nao deu certo subir os 2 apps).

--- meus-exemplos/dois-apps-no-mesmo-container/start.sh
+++ meus-exemplos/dois-apps-no-mesmo-container/start.sh
@@ -4,17 +4,18 @@ apt-get update
 apt-get install -y git
 apt-get install -y wget
 apt-get install -y unzip
+apt-get install -y net-tools
 wget https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz && \
 	tar xvf openjdk-17_linux-x64_bin.tar.gz && \
-	mv jdk-17 /opt/ && \
-	echo 'export JAVA_HOME=/opt/jdk-17' >> /root/.bashrc && \
-	echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /root/.bashrc && \
-	source /root/.bashrc
+	mv jdk-17 /opt/
+#	echo 'export JAVA_HOME=/opt/jdk-17' >> /root/.bashrc && \
+#	echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /root/.bashrc && \
+#	source /root/.bashrc
 git clone https://github.com/brunosantanati/docker.git && \
 	wget http://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.zip && \
-	unzip apache-maven-3.8.3-bin.zip && \
-	echo 'export PATH=$PATH:/root/apache-maven-3.8.3/bin' >> /root/.bashrc && \
-	source /root/.bashrc
+	unzip apache-maven-3.8.3-bin.zip
+#	echo 'export PATH=$PATH:/root/apache-maven-3.8.3/bin' >> /root/.bashrc && \
+#	source /root/.bashrc
 #cd /root/docker/meus-exemplos/dois-apps-no-mesmo-container/app2 && \
 #	/root/apache-maven-3.8.3/bin/mvn clean install
 #	mvn clean install && \
